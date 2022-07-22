da372de3ddf92fa9ee91f4f43c57e6dfaf58a717
VRGenius <101291974+VRGenius@users.noreply.github.com>
2022-07-22 09:10:54 +0300

    Update script.py

--- script.py
+++ script.py
@@ -6,23 +6,21 @@ response = requests.get(url)
 if response.status_code == requests.codes.ok:
     jsonResponse = response.json()  # the response is a JSON
     #the JSON is encoded in base 64, hence decode it
-    content = base64.b64decode(jsonResponse['content'])
+    contentb = base64.b64decode(jsonResponse['content'])
+    jsonString = contentb.decode('utf-8')
+    content = json.loads(jsonString)
 else:
     raise Exception ('Content was not found')
 
-    for cell in content['cells']:
-        source = 0
-        if len(cell['source']) == 0:
-            break
+for cell in content['cells']:
+    source = 0
+    if len(cell['source']) == 0:
+        break
 
-        if cell['cell_type'] == 'code':
-            try:
-                source = ''.join(line for line in cell['source'] if not line.startswith('%'))
-                exec(source, globals(), locals())
-                my_output = f"No error in " + str(cell['execution_count']) + " cell"
-                print(f"::set-output name=myOutput::{my_output}")
-                my_output = 0
-            except Exception as e:
-                my_output = f"Error in " + str(cell['execution_count']) + " cell"
-                print(f"::set-output name=myOutput::{my_output}")
-                my_output = 0
+    if cell['cell_type'] == 'code':
+        try:
+            source = ''.join(line for line in cell['source'] if not line.startswith('%'))
+            exec(source, globals(), locals())
+            print ("No error in " + str(cell['execution_count']) + " cell")
+        except Exception as e:
+            print ("Error in " + str(cell['execution_count']) + " cell")
